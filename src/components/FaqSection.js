b07b74db60e4b4453e40d3a3d040f34f516b7fde
Jay Pante <jaypante12@gmail.com>
2022-11-15 13:54:48 +0800

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+        <h2>Any Question <span>FAQ</span></h2>
+        <div className="question">
+            <h4>How Do I Start?</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Aspernatur modi ducimus facere impedit odit repellat doloremque consequatur? Ullam iste aspernatur cum sapiente numquam. Eligendi, ab veniam exercitationem et vitae ut?</p>
+
+            </div>
+        </div>
+        <div className="question">
+            <h4>Daily Schedule</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Aspernatur modi ducimus facere impedit odit repellat doloremque consequatur? Ullam iste aspernatur cum sapiente numquam. Eligendi, ab veniam exercitationem et vitae ut?</p>
+
+            </div>
+        </div>
+        <div className="question">
+            <h4>Different payment methods</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Aspernatur modi ducimus facere impedit odit repellat doloremque consequatur? Ullam iste aspernatur cum sapiente numquam. Eligendi, ab veniam exercitationem et vitae ut?</p>
+
+            </div>
+        </div>
+        <div className="question">
+            <h4>What products do you offer?</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Aspernatur modi ducimus facere impedit odit repellat doloremque consequatur? Ullam iste aspernatur cum sapiente numquam. Eligendi, ab veniam exercitationem et vitae ut?</p>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default FaqSection
