cb8054063243770311882bd8438ded43a7ff9ade
Jay Pante <jaypante12@gmail.com>
2022-11-25 09:22:40 +0800

    6 styled

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,43 +1,76 @@
 import React from 'react'
+import styled from 'styled-components'
+import { About } from '../styles'
+
 
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <Faq>
         <h2>Any Question <span>FAQ</span></h2>
         <div className="question">
             <h4>How Do I Start?</h4>
             <div className="answer">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Aspernatur modi ducimus facere impedit odit repellat doloremque consequatur? Ullam iste aspernatur cum sapiente numquam. Eligendi, ab veniam exercitationem et vitae ut?</p>
+                <p>Lorem ipsum dolor sit amet  </p>
+                <p>     Aspernatur modi ducimus facere impedit odit repellat doloremque consequatur? Ullam iste aspernatur cum sapiente numquam. </p>
 
             </div>
+            <div className="faq-line"></div>
         </div>
         <div className="question">
             <h4>Daily Schedule</h4>
             <div className="answer">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Aspernatur modi ducimus facere impedit odit repellat doloremque consequatur? Ullam iste aspernatur cum sapiente numquam. Eligendi, ab veniam exercitationem et vitae ut?</p>
+                <p>Lorem ipsum dolor sit amet </p>
+                <p>  Aspernatur modi ducimus facere impedit odit repellat doloremque consequatur? Ullam iste aspernatur cum sapiente numquam. </p>
 
             </div>
+            <div className="faq-line"></div>
         </div>
         <div className="question">
             <h4>Different payment methods</h4>
             <div className="answer">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Aspernatur modi ducimus facere impedit odit repellat doloremque consequatur? Ullam iste aspernatur cum sapiente numquam. Eligendi, ab veniam exercitationem et vitae ut?</p>
+                <p>Lorem ipsum dolor sit amet  </p>
+                <p>  Aspernatur modi ducimus facere impedit odit repellat doloremque consequatur? Ullam iste aspernatur cum sapiente numquam. </p>
 
             </div>
+            <div className="faq-line"></div>
         </div>
         <div className="question">
             <h4>What products do you offer?</h4>
             <div className="answer">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Aspernatur modi ducimus facere impedit odit repellat doloremque consequatur? Ullam iste aspernatur cum sapiente numquam. Eligendi, ab veniam exercitationem et vitae ut?</p>
+                <p>Lorem ipsum dolor sit amet </p>
+                <p>Aspernatur modi ducimus facere impedit odit repellat doloremque consequatur? Ullam iste aspernatur cum sapiente numquam. </p>
 
             </div>
+            <div className="faq-line"></div>
         </div>
-    </div>
+    </Faq>
   )
-}
+};
+
+const Faq = styled(About)`
+    display: block;
+    span{
+        display: block;
+    }
+    h2{
+        padding-bottom: 2rem;
+        font-weight: lighter;
+
+    }
+    .faq-line{
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .answer{
+        padding: 2rem 0rem;
+        p{
+            padding:1rem 0rem;
+
+        }
+    }
+`;
+
 
 export default FaqSection
